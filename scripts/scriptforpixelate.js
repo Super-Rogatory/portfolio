624af1981d52a3aac9740b7c18432cddcb86a2ae
super-rogatory <chukwudi10@gmail.com>
2021-06-18 14:58:58 -0700

    Added fixes to Pixelate, 1.0

--- scripts/scriptforpixelate.js
+++ scripts/scriptforpixelate.js
@@ -1,7 +1,13 @@
 let color = "red"; //global variable to control the color on LINE 10-12
+let col = 20; // Used in the makeRow function. Want to scale..
+let addToCol = 0; // every time we add data cells to the column, we need to keep count of how many data cells are needed, as when add a row is called, it isn't offset.
 let isScrollAvailable = false;
 const addRowButton = document.getElementById("add-row");
+const removeRowButton = document.getElementById("remove-row");
+const addColumnButton = document.getElementById("add-column");
+const removeColumnButton = document.getElementById("remove-column");
 const fillColorButton = document.getElementById("fill-color");
+const fillRemainingButton = document.getElementById("fill-remaining");
 const clearColorButton = document.getElementById("clear-color");
 const table = document.querySelector("table");
 const select = document.querySelector("select");
@@ -15,9 +21,13 @@ const select = document.querySelector("select");
 */
 // Makes sure that the DOM is loaded before manipulation.
 window.addEventListener("DOMContentLoaded", function () {
-  alert("Game Loaded. Pick a color!");
-  addRowButton.addEventListener("click", makeRow); // LINE 34 - 41 function definition
+  // alert("Game Loaded. Pick a color!");
+  addRowButton.addEventListener("click", makeRow); // LINE 43 - 50 function definition
+  removeRowButton.addEventListener("click", removeRow);
+  addColumnButton.addEventListener("click", addColumn);
+  removeColumnButton.addEventListener("click", removeColumn);
   fillColorButton.addEventListener("click", fillTable);
+  fillRemainingButton.addEventListener("click", fillNonColoredCells);
   clearColorButton.addEventListener("click", clearTable);
   table.addEventListener("click", colorize);
   table.addEventListener("mousedown", function () {
@@ -36,11 +46,35 @@ window.addEventListener("DOMContentLoaded", function () {
 // creates rows on click
 function makeRow() {
   const newTr = document.createElement("tr"); // creates a table row
-  for (let i = 0; i < 20; i++) {
+  // we need to create consistent rows. without addToCol, our rows will be displaced upon new add a row.
+  col += addToCol;
+  for (let i = 0; i < col; i++) {
     const newTd = document.createElement("td"); // created some table data
     newTr.appendChild(newTd);
   }
+  addToCol = 0; // the default SHOULD be zero. We don't want to resize our rows if addColumn button was never clicked.
   table.appendChild(newTr);
+}
+function removeRow() {
+  const lastRow = table.lastChild;
+  if(lastRow) table.removeChild(lastRow); // an error will be thrown if we remove a child from an element with no child
+}
+function addColumn(){
+  addToCol++; // we have added to an existing row, x amount of times.
+  const rows = Array.from(table.getElementsByTagName("tr"));
+  for(const index in rows){
+    // adding data cells to every row that exists in the table so far.
+    const newTd = document.createElement("td"); // has to be inside the loop
+    rows[index].appendChild(newTd);
+  }
+}
+function removeColumn(){
+  addToCol--;
+  const rows = Array.from(table.getElementsByTagName("tr"));
+  for(const index in rows){
+    const lastTd = rows[index].lastChild
+    if(lastTd) rows[index].removeChild(lastTd);
+  }
 }
 // adds color to the table data cell element, turns cell on or off (toggle) made on if it has color or not
 function colorize(event) {
@@ -76,10 +110,8 @@ function clearTable() {
     cell.className = "";
   });
 }
-/*
-    Add the ability to fill the whole grid with one color - DONE
-    Add the ability to fill any non-colored cells with one color
-    Add the ability to clear the grid - DONE
-    Add controls for the user to adjust the number of columns
-    Add controls for the user add/remove rows and columns
-*/
+function fillNonColoredCells() {
+  const cells = Array.from(document.getElementsByTagName("td"));
+  cells.forEach((cell) => cell.className === "" ? cell.className = color : "");
+}
+
